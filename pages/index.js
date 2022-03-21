5c344b556fc82e4a0a86e682936792b61f01581c
Sam S <sammi12006@gmail.com>
2022-03-21 13:28:00 -0600

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className='flex flex-col items-center justify-center w-full h-screen'>
+      <h1 className='text-3xl'>The Cat App</h1>
+    </div>
+  )
+}
