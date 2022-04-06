7ad203a07eaeab81728dc42e173c4407fd19ac51
sammi12006 <sammi12006@gmail.com>
2022-04-06 17:28:50 -0600

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
         <Image
-          class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+          className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
           src="/placeholder.png"
           alt="Woman's Face"
           width="64"
