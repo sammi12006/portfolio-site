501650f962042c8ff9f6c420c2d7f579b30a96bf
sammi12006 <sammi12006@gmail.com>
2022-04-06 17:15:51 -0600

    Added placeholder and canged index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <>
       <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-        <img
+        <Image
           class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
-          src="/img/erin-lindford.jpg"
+          src="/placeholder.png"
           alt="Woman's Face"
+          width="64"
+          height="64"
         />
         <div class="text-center space-y-2 sm:text-left">
           <div class="space-y-0.5">
